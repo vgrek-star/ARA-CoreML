19d84713a02948ad9b1a79f3b1a48c3cbf74c422
Volodymyr Grek <vgrek@star.global>
2021-07-13 07:26:55 +0300

    Added CocoaPods + some warnings removal

--- ARA-CoreML/YOLO.swift
+++ ARA-CoreML/YOLO.swift
@@ -3,118 +3,120 @@ import UIKit
 import CoreML
 
 class YOLO {
-  public static let inputWidth = 416
-  public static let inputHeight = 416
-  public static let maxBoundingBoxes = 10
-
-  // Tweak these values to get more or fewer predictions.
-  let confidenceThreshold: Float = 0.6
-  let iouThreshold: Float = 0.6
-
-  struct Prediction {
-    let classIndex: Int
-    let score: Float
-    let rect: CGRect
-  }
-
-  //let model = Yolov3()
-  //let model = model_b405_new()
-let model = yolo_v3_tiny_trained()
-  public init() { }
-
-  public func predict(image: CVPixelBuffer) throws -> [Prediction] {
-    //if let output = try? model.prediction(input1: image) {
-    if let output = try? model.prediction(inputs: image) {
-
-        //return computeBoundingBoxes(features: [output.output1, output.output2, output.output3])
-        return computeBoundingBoxes(features: output.predictions)
-    } else {
-      return []
+    public static let inputWidth = 416
+    public static let inputHeight = 416
+    public static let maxBoundingBoxes = 10
+    
+    // Tweak these values to get more or fewer predictions.
+    let confidenceThreshold: Float = 0.6
+    let iouThreshold: Float = 0.6
+    
+    struct Prediction {
+        let classIndex: Int
+        let score: Float
+        let rect: CGRect
     }
-  }
-
-  public func computeBoundingBoxes( features: MLMultiArray) -> [Prediction] {
- 
-
-    var predictions = [Prediction]()
-
-    let blockSize: Float = 32
-    let numClasses = 5
-    var boxesPerCell = features.count / (numClasses+5)
-    boxesPerCell = 3
     
-    var increment = numClasses+5
-
+    //let model = Yolov3()
+    //let model = model_b405_new()
+    let model = yolo_v3_tiny_trained()
     
-    var anchors2 = [(81, 82), (135, 169), (344, 319)]
-
-
+    public init() { }
     
-    
-    func offset(_ channel: Int, _ x: Int, _ y: Int) -> Int {
-      return channel + y + x
+    public func predict(image: CVPixelBuffer) throws -> [Prediction] {
+        //if let output = try? model.prediction(input1: image) {
+        if let output = try? model.prediction(inputs: image) {
+            
+            //return computeBoundingBoxes(features: [output.output1, output.output2, output.output3])
+            return computeBoundingBoxes(features: output.predictions)
+        } else {
+            return []
+        }
     }
-    var total_bbox = (features.count) / (numClasses + 5)
     
-
-    for j in 0..<total_bbox {
+    public func computeBoundingBoxes( features: MLMultiArray) -> [Prediction] {
+        
+        
+        var predictions = [Prediction]()
+        
+        let blockSize: Float = 32
+        let numClasses = 5
+        var boxesPerCell = features.count / (numClasses+5)
+        boxesPerCell = 3
+        
+        let increment = numClasses+5
+        
+        
+        var anchors2 = [(81, 82), (135, 169), (344, 319)]
         
         
         
-    var tx = Float(features[0+(increment*j)])
         
-    var ty = Float(features[1+(increment*j)])
-    var tw = Float(features[2+(increment*j)])
-                        //print("pointer : \(3+(11*i))")
-                        //print("th : \(features[14336])")
-    var th = Float(features[3+(increment*j)])
+        func offset(_ channel: Int, _ x: Int, _ y: Int) -> Int {
+            return channel + y + x
+        }
+        let total_bbox = (features.count) / (numClasses + 5)
         
         
-    //tx  = tx/416.0 * (416.0)
-    let tc = Float(features[4+(increment*j)])
-    let confidence = sigmoid(tc)
-   
-          //image sizes : 480.0 , 640.0
-     
-    var class_probs = [Float](repeating: 0, count: numClasses)
+        for j in 0..<total_bbox {
+            
+            
+            
+            let tx = Float(truncating: features[0+(increment*j)])
+            
+            let ty = Float(truncating: features[1+(increment*j)])
+            let tw = Float(truncating: features[2+(increment*j)])
+            //print("pointer : \(3+(11*i))")
+            //print("th : \(features[14336])")
+            let th = Float(truncating: features[3+(increment*j)])
+            
+            
+            //tx  = tx/416.0 * (416.0)
+            let tc = Float(truncating: features[4+(increment*j)])
+            let confidence = sigmoid(tc)
+            
+            //image sizes : 480.0 , 640.0
+            
+            var class_probs = [Float](repeating: 0, count: numClasses)
+            
+            
+            for c in 0..<numClasses {
+                class_probs[c] = Float(truncating: features[5+c+(increment*j)])
+            }
+            
+            let (detectedClass, bestClassScore) = class_probs.argmax()
+            // print("tc :  \(tc),confidence : \(confidence)")
+            // print("confidence, detectedClass, bestClassScore : ",confidence, detectedClass, bestClassScore)
+            let confidenceInClass = bestClassScore * confidence
+            // print("confidenceInClass  : ",confidenceInClass)
+            
+            if confidenceInClass >= confidenceThreshold {
+                let rect = CGRect(x: CGFloat(tx)  - (CGFloat(tw)/2), y: CGFloat(ty) - (CGFloat(th)/2),
+                                  width: CGFloat(tw), height: CGFloat(th))
+                
+                let prediction = Prediction(classIndex: detectedClass,score:    confidenceInClass,rect: rect)
+                predictions.append(prediction)
+            }
+        }
+        // }
+        let filtered = nonMaxSuppression(boxes: predictions, limit: 20, threshold: iouThreshold)
+        
+        print(filtered)
         
         
-    for c in 0..<numClasses {
-        class_probs[c] = Float(features[5+c+(increment*j)])
+        
+        // We already filtered out any bounding boxes that have very low scores,
+        // but there still may be boxes that overlap too much with others. We'll
+        // use "non-maximum suppression" to prune those duplicate bounding boxes.
+        
+        if (predictions.count <=  5){
+            return  predictions
+        }
+        else {
+            return filtered
+        }
     }
-        
-    let (detectedClass, bestClassScore) = class_probs.argmax()
-   // print("tc :  \(tc),confidence : \(confidence)")
-    // print("confidence, detectedClass, bestClassScore : ",confidence, detectedClass, bestClassScore)
-    let confidenceInClass = bestClassScore * confidence
-   // print("confidenceInClass  : ",confidenceInClass)
-        
-    if confidenceInClass >= confidenceThreshold {
-        let rect = CGRect(x: CGFloat(tx)  - (CGFloat(tw)/2), y: CGFloat(ty) - (CGFloat(th)/2),
-                                    width: CGFloat(tw), height: CGFloat(th))
-        
-        let prediction = Prediction(classIndex: detectedClass,score:    confidenceInClass,rect: rect)
-        predictions.append(prediction)
-    }
-    }
-   // }
-    let filtered = nonMaxSuppression(boxes: predictions, limit: 20, threshold: iouThreshold)
-        
-   print(filtered)
+    //nonMaxSuppression(boxes: predictions, limit: YOLO.maxBoundingBoxes, threshold: iouThreshold)
     
-    
-    
-    // We already filtered out any bounding boxes that have very low scores,
-    // but there still may be boxes that overlap too much with others. We'll
-    // use "non-maximum suppression" to prune those duplicate bounding boxes.
-    
-    if (predictions.count <=  5){
-    return  predictions
-    }
-    else {
-    return filtered
-    }
-    }
-        //nonMaxSuppression(boxes: predictions, limit: YOLO.maxBoundingBoxes, threshold: iouThreshold)
-  
 }
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'ARA-CoreML' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ARA-CoreML
+  #pod 'ZIPFoundation', '~> 0.9'
+
+end
